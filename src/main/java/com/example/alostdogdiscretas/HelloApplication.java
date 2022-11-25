18c57498794e6cd6cad268852315b5a15ccdf9f1
juanjose <juanjolo12@hotmail.com>
2022-11-24 21:07:55 -0500

    Falta Bfs

--- src/main/java/com/example/alostdogdiscretas/HelloApplication.java
+++ src/main/java/com/example/alostdogdiscretas/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.alostdogdiscretas;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,11 +11,21 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load() );
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        showWindow("Mainfx.fxml");
+    }
+
+    public static void showWindow(String fxml)  {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(fxml));
+
+            Parent node = fxmlLoader.load();
+            Scene scene = new Scene(node);
+            Stage window = new Stage();
+            window.setScene(scene);
+            window.show();
+        }catch (IOException ex){
+            ex.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
